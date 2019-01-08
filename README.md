b6f7fe647a8eac628379cd29719e0bcb9cb20f60
yao <yao@yaos-mbp.osustaff.okstate.edu>
2019-01-08 12:09:10 -0600

    autoencoder

---
+++ README.md
@@ -0,0 +1,7 @@
+### A demo about generating images using autoencoder
+
+A simple autoencoder uses fully connected network as encoder and decoder.
+
+
+The encoder output is show as follows.
+![Generated images](generated_images.png)
---
+++ autoencoder.py
@@ -0,0 +1,153 @@
+import os
+
+import torch
+import torchvision
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+
+if not os.path.exists('./reconstruction'):
+    os.mkdir('./reconstruction')
+
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+num_epochs = 10
+batch_size = 128
+learning_rate = 1e-3
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+dataset = MNIST('./data', transform=img_transform, download=True)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True), 
+            nn.Linear(64, 12), 
+            nn.ReLU(True), 
+            nn.Linear(12, 2))
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(2, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True), nn.Linear(128, 28 * 28), nn.Tanh())
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+class DecoderPart(nn.Module):
+    def __init__(self):
+        super(DecoderPart, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True), 
+            nn.Linear(64, 12), 
+            nn.ReLU(True), 
+            nn.Linear(12, 2))
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(2, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True), nn.Linear(128, 28 * 28), nn.Tanh())
+
+    def forward(self, x):
+        x = self.decoder(x)
+        return x
+      
+# Train autoencoder
+model = autoencoder().cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(
+    model.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+print('Training autoencoder...')
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data
+        img = img.view(img.size(0), -1)
+        img = Variable(img).cuda()
+
+        output = model(img)
+        loss = criterion(output, img)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print('epoch [{}/{}], loss:{:.4f}'
+          .format(epoch + 1, num_epochs, loss.item()))
+    if epoch % 10 == 0:
+        pic = to_img(output.cpu().data)
+        save_image(pic, './reconstruction/image_{}.png'.format(epoch))
+
+torch.save(model.state_dict(), './autoencoder.pth')
+
+
+# Initialize decoder
+print('Initializing decoder...')
+model_decoder = AutoDecoder().cuda()
+model_decoder.load_state_dict(torch.load('./sim_autoencoder.pth'))
+
+
+# Generate images by tuning two dimensions
+print('Generating...')
+num_x = 20
+num_y = 20
+im_two_features = np.zeros((28*num_x, 28*num_y))
+
+upper_bound = 16
+lower_bound = -16
+
+for idx_i, i in enumerate(np.linspace(lower_bound,upper_bound,num_x)):
+  for idx_j, j in enumerate(np.linspace(lower_bound,upper_bound,num_y)):
+    out_gen = model_decoder(Variable(torch.tensor([[i, j]])).cuda())
+
+    im_gen = out_gen.detach().cpu().numpy().reshape(28,28)
+    im_two_features[idx_i*28:(idx_i+1)*28, idx_j*28:(idx_j+1)*28] = im_gen
+    
+cv2.imwrite('tuning_two_features.jpg', im_two_features)
+
+
+# Generate images by tuning one dimension
+num_x = 1
+num_y = 20
+im_single_feature = np.zeros((28*num_x, 28*num_y))
+
+upper_bound = 16
+lower_bound = -16
+
+for idx_j, j in enumerate(np.linspace(lower_bound,upper_bound,num_y)):
+    out_gen = model_decoder(Variable(torch.tensor([[j, 30]])).cuda())
+
+    im_gen = out_gen.detach().cpu().numpy().reshape(28,28)
+    im_single_feature[0:28, idx_j*28:(idx_j+1)*28] = im_gen
+    
+cv2.imwrite('tuning_single_feature.jpg', im_single_feature)
+
+print("Finished")
